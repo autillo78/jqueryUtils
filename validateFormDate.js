e675dad9ba2fac874df2f25cb27b931f353bbd29
Angel <angelr@clickandgo.com>
2020-06-26 09:12:35 +0100

    version 1.0 done

--- validateFormDate.js
+++ validateFormDate.js
@@ -1,10 +1,13 @@
 
 $(function () {
 
+	yearMin = new Date("2010").getFullYear()
+	
+
 	$("#send").click(checkForm)
 
 	
-	$("#date").on("keydown, click", removeErrors)
+	$("#date").change(validateDateOnChange)
 
 })
 
@@ -13,23 +16,53 @@ function checkForm () {
 
 	//check date
 	if ($("#date").val() === "") {
-		showError("#date", "enter a date")
+
+		showErrors($("#date"), "enter a date")
 
 		return false
 	}
 }
 
 
-function showError( id, message ){
+function showErrors (item, message) {
 
-	$(id).focus().addClass("error");
-
-	$("#errorMessage").text(message).hide().fadeIn(800);
+	item.focus().addClass("error")
+	$("#errorMessage").text(message).hide().fadeIn(800)
 }
 
 
-function removeErrors(){
+function removeErrors (item) {
 
-	$(this).removeClass("error");
-	$("#errorMessage").text("");
+	item.removeClass("error")
+	$("#errorMessage").text("")
+}
+
+
+function validateDateOnChange () {
+
+	console.log('pass')
+
+	var date =  new Date($(this).val())
+
+	var day = date.getDate()
+	var month = date.getMonth()
+	var year = date.getFullYear()
+
+	if (year > yearMin) {
+
+		var currentYear = new Date().getFullYear()
+
+		if (day >= 1 && day <= 31
+			&& month >= 1 && month <= 12
+			&& year <= currentYear) {
+
+			removeErrors($(this))
+
+		} else {
+
+			showErrors($(this), "you are out of the date range") 
+		}
+
+	}
+
 }
